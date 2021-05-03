fbf2e1086dc8e453f1d4bd232c50418550e6f990
Casey Wood <caseywood252@gmail.com>
2021-05-03 20:01:04 +0000

    ice cream assignement

---
+++ ice-cream.js
@@ -0,0 +1,19 @@
+// // actioning arrays project for LHL
+
+// const iceCreamFlavours = ["chocolate", "vanilla", "cookies and cream", "rocky road", "strawberry"];
+// iceCreamFlavours.push('root beer')
+
+
+// console.log(iceCreamFlavours);
+
+// console.log(iceCreamFlavours[0]);
+// console.log(iceCreamFlavours[-1]);
+// console.log(iceCreamFlavours.length);
+
+const seasons = ["spring", "summer", "fall"];
+const length = seasons.length;
+
+seasons.push("winter");
+
+console.log(length);
+console.log(seasons.length);
